6c0b911db624047677601dbfd9bf7b9b832b03ab
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-04-15 21:38:47 -0500

    updated js to render entries to html page

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,34 +1,60 @@
 console.log('Hello World!');
 
-const allJournalEntries = [];
+const journalEntries = [
+    {
+        date: 'April 11, 2019',
+        conceptCovered: 'Java Objects/Functions',
+        content: 'Hello World!',
+        mood: 'happy'
+    },
+    {
+        date: 'April 10, 2019',
+        conceptCovered: 'JavaScript Functions',
+        content: 'I am a second journal entry!',
+        mood: 'happy'
+    },
+    {
+        date: 'April 9, 2019',
+        conceptCovered: 'JavaScript Functions',
+        content: 'I am a third journal entry!',
+        mood: 'happy'
+    },
+    {
+        date: 'April 8, 2019',
+        conceptCovered: 'Group Projects',
+        content: 'I know how to work in a group!',
+        mood: 'happy'
+    }
+]
 
-const journalEntry1 = {
-    date: 'April 11, 2019',
-    conceptCovered: 'Java Objects/Functions',
-    content: 'Hello World!',
-    mood: 'happy'
+const entryLog = document.querySelector('.entryLog');
+
+const makeJournalEntryComponent = (journalEntry) => {
+    return `            <div>
+                <h1>${journalEntry.conceptCovered}</h1>
+                <h3>${journalEntry.date}</h3>
+                <h3>Mood: ${journalEntry.mood}</h3>
+                <p>${journalEntry.content}</p>
+            </div>`
 }
 
-const journalEntry2 = {
-    date: 'April 10, 2019',
-    conceptCovered: 'JavaScript Functions',
-    content: 'I am a second journal entry!',
-    mood: 'happy'
+// FIRST METHOD / FOREACH LOOP ON ARRAY
+const renderJournalEntries = (entries) => {
+    journalEntries.forEach(function(entry){
+        let x = makeJournalEntryComponent(entry);
+        entryLog.innerHTML += x;
+        console.log(x);
+    })
 }
 
-const journalEntry3 = {
-    date: 'April 9, 2019',
-    conceptCovered: 'JavaScript Functions',
-    content: 'I am a third journal entry!',
-    mood: 'happy'
-}
-const journalEntry4 = {
-    date: 'April 8, 2019',
-    conceptCovered: 'Group Projects',
-    content: 'I know how to work in a group!',
-    mood: 'happy'
-}
-
-allJournalEntries.push(journalEntry1, journalEntry2, journalEntry3, journalEntry4);
-console.log('journal', allJournalEntries);
+// ALTERNATE METHOD / FOR LOOP -----------------------------------------------------
+// const renderJournalEntries = (entries) => {
+//     for (let i = 0; i < entries.length; i++){
+//         let entry = makeJournalEntryComponent(entries[i]);
+//         entryLog.innerHTML += entry;
+//         console.log(entry);
+//     }
+// }
+// Invoke the render function
+renderJournalEntries(journalEntries);
 
