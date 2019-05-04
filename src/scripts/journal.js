1097cd6c6f5a11bb080d2130854b77950ddab889
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-05-03 20:29:08 -0500

    deleted old code

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -6,7 +6,6 @@ const journalText = document.querySelector("#journaltext");
 const journalMood = document.querySelector("#mood");
 const journalConcept = document.querySelector("#journalConcept");
 const journalDate = document.querySelector("#journalDate");
-console.log("journal mood: ",journalMood.value);
 
 function newjournalObject() {
     const newJournal = {
@@ -22,51 +21,3 @@ document.querySelector("#submit").addEventListener("click", function () {
     event.preventDefault();
     newjournalObject();
 });
-
-
-
-
-
-
-// const entryLog = document.querySelector('.entryLog');
-
-// const makeJournalEntryComponent = (journalEntry) => {
-//     return `<div>
-//                 <h1>${journalEntry.conceptCovered}</h1>
-//                 <h3>${journalEntry.date}</h3>
-//                 <h3>Mood: ${journalEntry.mood}</h3>
-//                 <p>${journalEntry.content}</p>
-//             </div>`
-// }
-
-// // Utilize fetch to retrieve posts from our JSON database 
-// fetch(`http://localhost:3000/journalEntries`)
-//     .then(journalEntries => journalEntries.json())
-//     .then(entries => {
-//         entries.forEach(entry => {
-//             console.table(entry);
-//             let postEntry = makeJournalEntryComponent(entry);
-//             entryLog.innerHTML += postEntry;
-//         })
-//     })
-
-// FIRST METHOD / FOREACH LOOP ON ARRAY
-// const renderJournalEntries = (entries) => {
-//     journalEntries.forEach(function(entry){
-//         let x = makeJournalEntryComponent(entry);
-//         entryLog.innerHTML += x;
-//         console.log(x);
-//     })
-// }
-
-// ALTERNATE METHOD / FOR LOOP -----------------------------------------------------
-// const renderJournalEntries = (entries) => {
-//     for (let i = 0; i < entries.length; i++){
-//         let entry = makeJournalEntryComponent(entries[i]);
-//         entryLog.innerHTML += entry;
-//         console.log(entry);
-//     }
-// }
-// Invoke the render function
-// renderJournalEntries(journalEntries);
-
