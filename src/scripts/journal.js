90038aa3771017cbc8dc8fa2f5c080cac0012103
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-05-03 20:23:53 -0500

    added ability to post new entries to api

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,26 +1,54 @@
 console.log('Hello World!');
 
-const entryLog = document.querySelector('.entryLog');
+API.getJournalEntries().then(renderJournalEntries);
 
-const makeJournalEntryComponent = (journalEntry) => {
-    return `<div>
-                <h1>${journalEntry.conceptCovered}</h1>
-                <h3>${journalEntry.date}</h3>
-                <h3>Mood: ${journalEntry.mood}</h3>
-                <p>${journalEntry.content}</p>
-            </div>`
-}
+const journalText = document.querySelector("#journaltext");
+const journalMood = document.querySelector("#mood");
+const journalConcept = document.querySelector("#journalConcept");
+const journalDate = document.querySelector("#journalDate");
+console.log("journal mood: ",journalMood.value);
 
-// Utilize fetch to retrieve posts from our JSON database 
-fetch(`http://localhost:3000/journalEntries`)
-    .then(journalEntries => journalEntries.json())
-    .then(entries => {
-        entries.forEach(entry => {
-            console.table(entry);
-            let postEntry = makeJournalEntryComponent(entry);
-            entryLog.innerHTML += postEntry;
-        })
-    })
+function newjournalObject() {
+    const newJournal = {
+        date: journalDate.value,
+        conceptCovered: journalConcept.value,
+        content: journalText.value,
+        mood: journalMood.value
+    };
+    API.saveNewEntry(newJournal).then(renderJournalEntries);
+};
+
+document.querySelector("#submit").addEventListener("click", function () {
+    event.preventDefault();
+    newjournalObject();
+});
+
+
+
+
+
+
+// const entryLog = document.querySelector('.entryLog');
+
+// const makeJournalEntryComponent = (journalEntry) => {
+//     return `<div>
+//                 <h1>${journalEntry.conceptCovered}</h1>
+//                 <h3>${journalEntry.date}</h3>
+//                 <h3>Mood: ${journalEntry.mood}</h3>
+//                 <p>${journalEntry.content}</p>
+//             </div>`
+// }
+
+// // Utilize fetch to retrieve posts from our JSON database 
+// fetch(`http://localhost:3000/journalEntries`)
+//     .then(journalEntries => journalEntries.json())
+//     .then(entries => {
+//         entries.forEach(entry => {
+//             console.table(entry);
+//             let postEntry = makeJournalEntryComponent(entry);
+//             entryLog.innerHTML += postEntry;
+//         })
+//     })
 
 // FIRST METHOD / FOREACH LOOP ON ARRAY
 // const renderJournalEntries = (entries) => {
